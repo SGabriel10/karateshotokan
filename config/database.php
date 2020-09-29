bc99e15fb60145ef924c0c8e934c7c7f3bb2d9c8
sgabriel <sergio.gabriel.el10@gmail.com>
2020-09-29 17:43:38 -0400

    update databaseasdf

--- config/database.php
+++ config/database.php
@@ -54,18 +54,16 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'mysql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"],"/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-            'strict' => true,
-            'engine' => null,
+            'driver' => 'pgsql',
+              'host' => env('DB_HOST', 'ec2-12-34-56-789.compute-1.amazonaws.com'),
+              'port' => env('DB_PORT', '5432'),
+              'database' => env('DB_DATABASE', 'd2k8ilkhu5hpmr'),
+              'username' => env('DB_USERNAME', 'wgxxsfwmxuyvyz'),
+              'password' => env('DB_PASSWORD', '49ded549d862723b669d527f6bb6aacd3d2d5544df784620c5c9fb1d8931f04b'),
+              'charset' => 'utf8',
+              'prefix' => '',
+              'schema' => 'public',
+              'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
