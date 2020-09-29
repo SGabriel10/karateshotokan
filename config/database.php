f25e3afeb26e98294a1c373b871f322536bc0382
sgabriel <sergio.gabriel.el10@gmail.com>
2020-09-29 17:11:35 -0400

    update database2

--- config/database.php
+++ config/database.php
@@ -38,6 +38,20 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
 
         'pgsql' => [
             'driver' => 'mysql',
@@ -48,9 +62,10 @@ return [
             'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes'=>true,
             'schema' => 'public',
             'sslmode' => 'prefer',
+            'strict' => true,
+            'engine' => null,
         ],
 
         'sqlsrv' => [
