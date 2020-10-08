2342c1fd67e165856b6d4244ecf38bd680179fa9
sgabriel <sergio.gabriel.el10@gmail.com>
2020-10-08 01:49:16 -0300

    mejora de vista de nosotros y actualizacion de estilos.css

--- resources/views/modules/nosotros.blade.php
+++ resources/views/modules/nosotros.blade.php
@@ -1,45 +1,35 @@
 @extends('pages/template')
 @section('seccion')
-<div class="about-section" id="about-id">
+<div class="about-section">
 <h2 style="text-align:center">Nuestro Equipo</h2>
-<div class="row" id='column-id'>
-  <div class="column">
-    <div class="card">
-    <img src="{{asset('juan.jpg')}}" id="card-targets">
-      <div class="container">
-        <h2>Juan R. Cubas</h2>
-        <p class="title">Presidente y Fundador</p>
-        <p>2000, CEO</p>
-        <p>juan@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
+<div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="{{asset('juan.jpg')}}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Juan R. Cubas</h5>
+      <p class="card-text">2000, CEO</p>
+      <p class="card-text"><small class="text-muted">juan@example.com</small></p>
+      <p><button class="button">Contacto</button></p>
     </div>
   </div>
-
-  <div class="column">
-    <div class="card">
-    <img src="{{asset('norma.jpg')}}"  id="card-targets"> 
-      <div class="container">
-        <h2>Norma</h2>
-        <p class="title">Directora Seda Poty</p>
-        <p>2000, CEO </p>
-        <p>norma@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
+  <div class="card">
+    <img class="card-img-top" src="{{asset('norma.jpg')}}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Norma Krug</h5>
+      <p class="card-text">Fundadora de Danza Reseda Poty</p>
+      <p class="card-text"><small class="text-muted">norma@example.com</small></p>
+      <p><button class="button">Contacto</button></p>
     </div>
   </div>
-
-  <div class="column">
-    <div class="card">
-    <img src="{{asset('build-page.jpg')}}" id="card-targets"> 
-      <div class="container">
-        <h2>Juanito Perez</h2>
-        <p class="title">Secretario</p>
-        <p>Secretario Financiero</p>
-        <p>juanito@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
+  <div class="card">
+    <img class="card-img-top" src="{{asset('build-page.jpg')}}" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Juan Perez</h5>
+      <p class="card-text">Secreatrio administrativo</p>
+      <p class="card-text"><small class="text-muted">juan.perez@example.com</small></p>
+      <p><button class="button">Contacto</button></p>
     </div>
   </div>
+</div>
 </div>
 @endsection
